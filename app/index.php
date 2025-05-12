cf409b4a6f998842fd86c6b0889c55fe45cf4c90
mchl-labs <72876787+mchl-labs@users.noreply.github.com>
2025-05-12 19:14:26 +0200

    refactor: simplify header structure by removing unnecessary elements

--- app/index.php
+++ app/index.php
@@ -8,39 +8,8 @@
 <body>
 <div class="header">
     <div class="container">
-        <div class="row">
-            <div class="col-md-5">
-                <div class="header-left">
-                    <div class="logo">
-                        <a href="index.html"><img src="images/logo.png" alt=""/></a>
-                    </div>
-                    <div class="menu">
-                        <a class="toggleMenu" href="#"><img src="images/nav.png" alt="" /></a>
-                        <ul class="nav" id="nav">
-                            <li><a href="index.html">Home</a></li>
-                            <li><a href="about.html">About</a></li>
-                            <li><a href="services.html">Services</a></li>
-                            <li><a href="blog.html">Blog</a></li>
-                            <li><a href="contact.html">Contact</a></li>
-                        </ul>
-                        <script type="text/javascript" src="js/responsive-nav.js"></script>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="logo-top">
-                    <h6>MilliPW</h6>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="header_right">
-                    <ul class="icon1 sub-icon1">
-                        <li><a href="login.html">Login</a></li>
-                        <li><a href="register.html">Register</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+        <h1 class="logo">Azienda ospedaliera</h1>
+        <h2 class="slogan">La tua salute è la nostra priorità</h2>
     </div>
 </div>
 <?php	
