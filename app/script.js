1cb612b8b8b295ac83f2f72927ff12a90777870d
Maurizuc <m.zucchelli7@studenti.unibg.it>
2025-06-10 16:56:08 +0200

    updated check icon

--- app/script.js
+++ app/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Dati dei path delle icone
     const iconPaths = {
         delete: 'M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z',
-        check: 'M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z'
+        check: 'M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z'
     };
 
     // Itera su ogni link di eliminazione trovato nella pagina
