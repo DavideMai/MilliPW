a73aad3108bb740910b375b197c9e7fdb9d59940
Maurizuc <m.zucchelli7@studenti.unibg.it>
2025-06-10 17:18:53 +0200

    nevermind

--- app/ospedale.php
+++ app/ospedale.php
@@ -349,7 +349,7 @@
 
                     echo "<td style='text-align: center;'>";
                     echo "<a href='?action=deleteconfirm&id=" . htmlspecialchars($thisId) . "'
-                    class='delete-confirm-link' data-original-href='?action=deleteconfirm&id=" . htmlspecialchars($thisId) . "' ><svg class='icon-path-data' xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 -960 960 960' width='24px'><path d='M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z'/></svg></a>";
+                    class='delete-confirm-link' data-original-href='?action=deleteconfirm&id=" . htmlspecialchars($thisId) . "' ><svg class='icon-path-data' xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 -960 960 960' width='24px' fill='#8B1A10'><path d='M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z'/></svg></a>";
                     echo "</td>";
 
                     echo "</tr>";                    
