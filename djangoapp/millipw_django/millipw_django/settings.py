b0ab15a7b7e7c83a549e5edccde60265d08cec84
mchl-labs <72876787+mchl-labs@users.noreply.github.com>
2025-06-27 16:17:00 +0200

    feat: add .gitignore, update settings for PostgreSQL, and include requirements for dotenv

--- djangoapp/millipw_django/millipw_django/settings.py
+++ djangoapp/millipw_django/millipw_django/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+from urllib.parse import urlparse, parse_qsl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -69,14 +71,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'millipw_django.wsgi.application'
 
+# Load environment variables from .env file
+
+load_dotenv()
+tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': tmpPostgres.path.replace('/', ''),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': 5432,
+        'OPTIONS': dict(parse_qsl(tmpPostgres.query)),
     }
 }
 
