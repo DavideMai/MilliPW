26f90f5d3834e1ac96f9021422a29c3bb5373f06
mchl-labs <72876787+mchl-labs@users.noreply.github.com>
2025-06-27 16:20:17 +0200

    feat: import os and configure message tags for Django messages framework

--- djangoapp/millipw_django/millipw_django/settings.py
+++ djangoapp/millipw_django/millipw_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 from dotenv import load_dotenv
 from urllib.parse import urlparse, parse_qsl
 
@@ -134,3 +135,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
